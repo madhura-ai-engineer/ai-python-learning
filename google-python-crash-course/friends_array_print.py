294935ba8d73939404ccc468ccaf3b7054bda3fa
Madhura Balasooriya <madhurarbalasooriya@gmail.com>
2026-01-15 09:17:23 +0530

    My First Friends Array Script

---
+++ google-python-crash-course/friends_array_print.py
@@ -0,0 +1,7 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+
+friends_set_two = ['Adam', 'Clara', 'Modas', 'Groge']
+for friend_two in friends_set_two:
+    print("Yo Yo "+ friend_two)
