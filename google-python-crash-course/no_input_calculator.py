266c3c94a5cff0514107cb91219db782e04fe68d
Madhura Balasooriya <madhurarbalasooriya@gmail.com>
2026-01-19 08:34:34 +0530

    no_input_calculator

--- google-python-crash-course/no_input_calculator.py
+++ google-python-crash-course/no_input_calculator.py
@@ -2,4 +2,8 @@ print(4+5)
 
 print(9*7)
 
-print(((2*7)/2)*3)
+print(((2*7)/2)*3) #/ - is division it will give floating value
+
+print(((2*7)//2)*3) #// - is floor division it will give integer value
+
+print(2**10) #//2 to the power of 10
